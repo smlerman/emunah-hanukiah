0842204d8a412991ecd5d97a183117eaf9c944e7
Scott Lerman <smlerman@gmail.com>
2016-12-30 00:44:57 -0500

    Update build_package.py to no longer copy scripts to different directories
    Update paths to other scripts in menorah_page.py

--- build_package.py
+++ build_package.py
@@ -6,39 +6,15 @@ import shutil
 import subprocess
 
 parser = argparse.ArgumentParser(description="Build a Raspbian .deb package for the Emunah Menorah")
-parser.add_argument("-d", "--tempdir", dest="tempdir", required=True, help="Temporary working directory for building the package; the directory must be empty or not exist")
+parser.add_argument("-d", "--tempdir", dest="tempdir", required=True, help="Temporary working directory for building the package; the directory must not exist")
 args = parser.parse_args()
 
 # Check that the temp working directory is clean
 if os.path.exists(args.tempdir):
-    if (not os.path.isdir(args.tempdir)) or (len(os.listdir(args.tempdir)) > 0):
-        raise Exception("The temp directory must be empty or not exist")
-else:
-    os.makedirs(args.tempdir)
+    raise Exception("The temp directory must be empty or not exist")
 
-# Copy files
-shutil.copytree("package_files/DEBIAN", os.path.join(args.tempdir, "DEBIAN"))
-shutil.copytree("package_files/etc", os.path.join(args.tempdir, "etc"), symlinks=True)
-
-bin_dir = os.path.join(args.tempdir, "usr/local/bin/emunah-menorah")
-os.makedirs(bin_dir)
-shutil.copy2("dimmer_control.py", bin_dir)
-
-html_dir = os.path.join(args.tempdir, "var/www/html/menorah")
-os.makedirs(html_dir)
-shutil.copy2("board_cleanup.py", html_dir)
-shutil.copy2("board_init.sh", html_dir)
-shutil.copy2("menorah_page.py", html_dir)
-os.chmod(os.path.join(html_dir, "board_cleanup.py"), 0o700)
-os.chmod(os.path.join(html_dir, "board_init.sh"), 0o700)
-
-light_state_dir = os.path.join(args.tempdir, "var/www/html/menorah/light_state")
-os.mkdir(light_state_dir)
-os.chmod(light_state_dir, 0o777)
-
-module_dir = os.path.join(args.tempdir, "usr/lib/python2.7/dist-packages")
-os.makedirs(module_dir)
-shutil.copy2("menorah_functions.py", module_dir)
+# Copy files to the temp directory
+shutil.copytree("package_files", args.tempdir, symlinks=True)
 
 # Build the package
 subprocess.check_call(["dpkg-deb", "--build", args.tempdir])
