0d38c3c4695c42e78d812a9eea1c71abe40cb272
Scott Lerman <smlerman@gmail.com>
2017-12-06 07:41:26 -0500

    Update Wii remote handler code
    Add code to turn on the next light when the remote is swung or the A button is pressed
    Add code to listen for a new connection after the remote is disconnected with the power button
    Add handler for receiving SIGQUIT from systemd
    Add systemd service for the Wii remote handler script
    Add package dependency for python-cwiid package
    Add postinst command to remove the default Apache virtual host
    Add postinst commands to enable and start the dimmer control and Wii remote handler services

--- package_files/usr/bin/emunah-menorah/wii_remote_handler.py
+++ package_files/usr/bin/emunah-menorah/wii_remote_handler.py
@@ -1,15 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import cwiid
+import signal
+import sys
 import time
+
+from menorah_functions import *
 
 class WiiRemote():
     # Accelerometer reading required to trigger a light change
     ACC_THRESHOLD = 230
     
+    remotes = {1: None, 2: None, 3: None, 4: None}
+    
+    last_message_time = time.time()
+    
+    @staticmethod
+    def listen():
+        WiiRemote.remotes[1] = WiiRemote()
+        WiiRemote.remotes[1].connect_remote()
+    
     def __init__(self):
         self.wiimote = None
-        self.last_message_time = time.time()
+        WiiRemote.last_message_time = time.time()
     
     def connect_remote(self):
         while self.wiimote is None:
@@ -58,13 +71,54 @@ class WiiRemote():
                 # If any of the accelerometer reading for any direction exceeds the threshold, trigger a light change
                 if (acc_x > WiiRemote.ACC_THRESHOLD) or (acc_y > WiiRemote.ACC_THRESHOLD) or (acc_z > WiiRemote.ACC_THRESHOLD):
                     # Check only once every 3 seconds
-                    if message_time > self.last_message_time + 3:
-                        self.last_message_time = message_time
-                        print("message_callback")
-                        print 'Acc Report: x=%d, y=%d, z=%d' % (message_data[cwiid.X], message_data[cwiid.Y], message_data[cwiid.Z])
+                    if message_time > WiiRemote.last_message_time + 3:
+                        WiiRemote.last_message_time = message_time
+                        WiiRemote.turn_on_next_light()
+            elif message_type == cwiid.MESG_BTN:
+                if message_data == cwiid.BTN_A:
+                    WiiRemote.last_message_time = message_time
+                    WiiRemote.turn_on_next_light()
+            elif message_type == cwiid.MESG_ERROR:
+                #if message_data == cwiid.ERROR_DISCONNECT:
+                WiiRemote.remotes[1].close()
+                del WiiRemote.remotes[1]
+                WiiRemote.listen()
+                return
+    
+    @staticmethod
+    def turn_on_next_light():
+        # Get the current light states
+        current_light_states = read_light_state_file()
+        
+        # Find the first light that's off and turn it on
+        next_light_found = False
+        for candle in range(0, len(current_light_states)):
+            if current_light_states[candle] == False:
+                current_light_states[candle] = True
+                next_light_found = True
+                break
+        
+        # If all of the lights are on, turn them all off
+        if not next_light_found:
+            current_light_states = new_light_states = [False, False, False, False, False, False, False, False, False]
+        
+        # Output the new states
+        write_light_state_file(current_light_states)
+        
+        # Send a reload command to the service
+        reload_service()
 
-wiiremote1 = WiiRemote()
-wiiremote1.connect_remote()
+# Handle SIGQUIT, sent by systemctl stop
+def sigquit_handler(signum, stack_frame):
+    for i in range(0, len(WiiRemote.remotes)):
+        WiiRemote.remotes[i].close()
+        del WiiRemote.remotes[i]
+    
+    sys.exit(0)
+
+signal.signal(signal.SIGQUIT, sigquit_handler)
+
+WiiRemote.listen()
 
 while True:
     time.sleep(0.1)
